0cdd15e606ab916492838d80257e108225f743c3
Amber Jain <jainanuamber@gmail.com>
2022-07-08 12:09:48 +0530

    uploaded

---
+++ test/testApp.swift
@@ -0,0 +1,18 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Amber Jain on 02/07/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+ 
